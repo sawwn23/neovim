1a3a92dc47b89993b35b4f6b288d997596e27e5c
sawwinnnaung <saw.wn@kernellix.com>
2023-01-04 22:43:28 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Requirements
+
+install neovim
+install npm
+
+# Installation
+
+Clone the repo to the .config/nvim
+
+```
+PackerInstall
+Mason
+```
+
+# Keybindings
+
+```
+Space key as leader
+
+use jk to exit insert mode
+
+leader sv to split vertically
+leader sh to split horizontally
+leader sx to close current window
+
+use control hjkl to move between windows
+
+leader sm for maximization
+
+leader e to open file explorer
+
+leader ff to find files
+leader fs for live grep
+
+leader gc to comment current line
+leader gcip comment/uncomment a paragraph
+leader gc4w comment/uncomment current line
+leader gc4j comment/uncomment 4 lines below the current line
+leader dic delete comment block
+leader gcic uncomment commented block
+
+
+```
+
+# customization
+
+change color scheme in core colorscheme.lua
+
+Add your own language
---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,47 @@
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+-- enable mason
+mason.setup()
+-- Enable the following language servers
+-- Feel free to add/remove any LSPs that you want here. They will automatically be installed
+local servers = { 'terraformls', 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'gopls', 'emmet_ls' }
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = servers,
+  -- auto-install configured servers (with lspconfig)
+  automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+  -- list of formatters & linters for mason to install
+  ensure_installed = {
+    "prettierd", -- bash formatter
+    "terraform_fmt", -- terraform formatter
+    "shfmt",
+    "fixjson",
+    "isort",
+    "black",
+    "stylua",
+    "flake8",
+    "eslint_d", -- ts/js linter
+  },
+  -- auto-install configured formatters & linters (with null-ls)
+  automatic_installation = true,
+})
